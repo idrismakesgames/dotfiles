f94feb33a197a31f61010c88c1d501389acced8c
Idris Hussain <idrismakesgame@gmail.com>
2025-06-24 10:51:33 +0100

    Updated abbreviations for fish@

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,3 +5,14 @@ fish_config theme choose "Rosé Pine"
 starship init fish | source
 set -gx EDITOR nvim
 set -gx VISUAL nvim
+set -x nvmrc_announce no 
+set -g fish_greeting
+
+abbr --add gco git checkout   
+abbr --add gac 'git add -A && git commit -m "ME-'  
+abbr --add fic nvim ~/.config/fish/config.fish 
+abbr --add kic nvim ~/.config/kitty/kitty.conf
+abbr --add nvc nvim ~/.config/nvim/init.lua
+abbr --add stc nvim ~/.config/starship.toml
+abbr --add c clear
+
