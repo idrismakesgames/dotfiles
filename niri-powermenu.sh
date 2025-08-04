761f1b1d0c04ee3ebde2d0f2235dcd8996e43833
Idris Hussain <idrismakesgames@gmail.com>
2025-08-04 13:20:50 +0100

    niri setup all sorted

---
+++ niri-powermenu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SELECTION="$(printf "1 - Log out\n2 - Reboot\n3 - Reboot to UEFI\n4 - Hard reboot\n5 - Shutdown" | fuzzel --dmenu -l 6 -p "Power Menu: ")"
+
+case $SELECTION in
+	*"Log out")
+		pkill niri;;
+	*"Reboot")
+		systemctl reboot;;
+	*"Reboot to UEFI")
+		systemctl reboot --firmware-setup;;
+	*"Hard reboot")
+		pkexec "echo b > /proc/sysrq-trigger";;
+	*"Shutdown")
+		systemctl poweroff;;
+esac
