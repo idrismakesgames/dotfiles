d8800f5a0dbc115ba45234affb659636ca6aa912
Idris Hussain <idrismakesgames@gmail.com>
2025-08-05 21:54:45 +0100

    nvim and yazi set up, defaults back to being arrows on layer which is the best way

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,5 @@
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Remap new movement keys: neio -> hjkl
+-- map('n', 'e', 'j', opts) -- move down
---
+++ nvim/lua/plugins/kanagawa.lua
@@ -0,0 +1,10 @@
+return {
+	"rebelot/kanagawa.nvim", 
+	config=function() 
+		require('kanagawa').setup({
+			compile=true
+		});
+		vim.cmd("colorscheme kanagawa");
+	end,
+	build = ":KanagawaCompile"
+}
