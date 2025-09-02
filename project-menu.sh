39c3c63204c61d4451123aa57a1c4d62351bbf76
Idris Hussain <idrismakesgames@gmail.com>
2025-09-02 08:50:41 +0100

    neovim config session added to the project list

--- project-menu.sh
+++ project-menu.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-SELECTION="$(printf "1 - Skimlinks\n2 - Notes\n3 - Full Stack\n4 - Website" | fuzzel --dmenu -l 5 -p "Power Menu: ")"
+SELECTION="$(printf "1 - Skimlinks\n2 - Notes\n3 - Neovim Config\n4 - Full Stack\n5 - Website" | fuzzel --dmenu -l 5 -p "Power Menu: ")"
 
 case $SELECTION in
 	*"Skimlinks")
 		~/Dotfiles/work-start.sh;;
 	*"Notes")
 		~/Dotfiles/notes-start.sh;;
+	*"Neovim Config")
+		~/Dotfiles/nvim-config-start.sh;;
 	*"Full Stack")
 		;;
 	*"Website")
