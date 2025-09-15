4ab4fdf5ef758c46536a6968d467f70cd8e1748e
Idris Hussain <idrismakesgames@gmail.com>
2025-09-15 14:24:16 +0100

    updated the projects dmeny setup

--- work-start.sh
+++ work-start.sh
@@ -8,3 +8,9 @@ sleep 0.5
 
 # Step 3: Run kitty with your session config
 kitty --session /home/idrish/.config/kitty/work-session.conf &
+# sleep 0.1
+# kitty --session /home/idrish/.config/kitty/skim-ocelot-session.conf &
+# sleep 0.1
+# kitty --session /home/idrish/.config/kitty/skim-helium-session.conf &
+# sleep 0.1
+# kitty --session /home/idrish/.config/kitty/skim-editor-tool-session.conf &
