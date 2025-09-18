9f031b95485eb541118bfaa6f19c90327aa44c94
Idris Hussain <idrismakesgames@gmail.com>
2025-09-18 13:17:13 +0100

    dotfiles updated. notes to add

--- notes-start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Step 1: Run kitty with your session config
-kitty --session /home/idrish/.config/kitty/notes-session.conf &
--- project-menu.sh
+++ project-menu.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-SELECTION="$(printf "1 - Skimlinks\n2 - Notes\n3 - Neovim Config\n4 - Full Stack\n5 - Website" | fuzzel --dmenu -l 5 -p "Power Menu: ")"
+SELECTION="$(printf "1 - Skimlinks\n2 - Neovim Config\n3 - Full Stack\n5 - Website" | fuzzel --dmenu -l 3 -p "Power Menu: ")"
 
 case $SELECTION in
 	*"Skimlinks")
 		~/Dotfiles/work-start.sh;;
-	*"Notes")
-		~/Dotfiles/notes-start.sh;;
 	*"Neovim Config")
 		~/Dotfiles/nvim-config-start.sh;;
 	*"Full Stack")
-		;;
-	*"Website")
 		;;
 esac
