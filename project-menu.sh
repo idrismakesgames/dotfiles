2620a808ee41b947520660e9cf0297068601084d
Idris Hussain <idrismakesgames@gmail.com>
2025-09-18 16:40:51 +0100

    notes and font adjusted for the terminal

--- project-menu.sh
+++ project-menu.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-SELECTION="$(printf "1 - Skimlinks\n2 - Neovim Config\n3 - Full Stack\n5 - Website" | fuzzel --dmenu -l 3 -p "Power Menu: ")"
+SELECTION="$(printf "1 - Skimlinks\n2 - Neovim Config\n3 - Notes\n4 - Full Stack" | fuzzel --dmenu -l 4 -p "Power Menu: ")"
 
 case $SELECTION in
 	*"Skimlinks")
 		~/Dotfiles/work-start.sh;;
 	*"Neovim Config")
 		~/Dotfiles/nvim-config-start.sh;;
+	*"Notes")
+		obsidian;;
 	*"Full Stack")
 		;;
 esac
